f46dccb2eb303a155492d4f17a7502c44d6701bc
robroblore <robroblore@yahoo.com>
2025-02-08 20:16:57 +0100

    auto commit

---
+++ BankWare/src/main/java/org/loreware/bankWare/BankWare.java
@@ -0,0 +1,18 @@
+package org.loreware.bankWare;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public final class BankWare extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
